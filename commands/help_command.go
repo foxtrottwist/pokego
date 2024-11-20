b08aa0f2b087d225071985e0c3bf437dc98f2908
Law Horne <hello@foxtrottwist.com>
2024-11-20 14:22:06 -0500

    Updated help command to print an ordered list and improved formatting.

--- commands/help_command.go
+++ commands/help_command.go
@@ -1,12 +1,30 @@
 package commands
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
 func helpCommand(c *config, args ...string) error {
 	c.print("\nWelcome to the PokéGo Pokédex!\nUsage:\n\n")
 
-	for _, cmd := range Commands() {
-		c.print(fmt.Sprintf("%s: %s\n", cmd.name, cmd.description))
+	cmds := Commands()
+	keys := make([]string, 0, len(cmds))
+	padding := 0
+
+	for key := range cmds {
+		if l := len(key); l > padding {
+			padding = len(key)
+		}
+		keys = append(keys, key)
+	}
+
+	sort.Strings(keys)
+
+	for _, key := range keys {
+		cmd, _ := cmds[key]
+		c.print(fmt.Sprintf("%s:%s%s\n", cmd.name, strings.Repeat(" ", padding*2-len(key)), cmd.description))
 	}
 
 	c.print("\n")
